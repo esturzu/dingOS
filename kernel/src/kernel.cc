0819d1cbaf005f03296502f5dbc654f030b9393d
Joshua Clark <joshuarc@utexas.edu>
2025-01-22 15:25:25 -0600

    Josh 2

--- kernel/src/kernel.cc
+++ kernel/src/kernel.cc
@@ -1,6 +1,16 @@
 #include "kernel.h"
 
+int testMain()
+{
+	int test = 1;
+	int test2 = 2;
+	int test3 = test * test2;
+	return test3;
+}
+
 extern "C" void kernelMain()
 {
-	while (1) {}
+	while (1) {
+		testMain();
+		}
 }
