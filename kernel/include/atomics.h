d13fea8bf03734356f557c6dd3e443b51cabd5d6
Joshua Clark <joshuarc@utexas.edu>
2025-01-31 14:32:18 -0600

    Fix asm naming

--- kernel/include/atomics.h
+++ kernel/include/atomics.h
@@ -1,11 +1,14 @@
 #ifndef ATOMICS_H
 #define ATOMICS_H
 
-extern long atomic_load(long* obj);
-extern void atomic_store(long* obj, long desired);
-extern long atomic_exchange(long* obj, long desired);
-// actually returns a 1 or 0 depending on result but for some reason bool wouldnt work for me
-extern int atomic_compare_exchange(long* obj, long* expected, long desired);
+
+extern "C"{
+    long atomic_load(long* obj);
+    void atomic_store(long* obj, long desired);
+    long atomic_exchange(long* obj, long desired);
+    // actually returns a 1 or 0 depending on result but for some reason bool wouldnt work for me
+    int atomic_compare_exchange(long* obj, long* expected, long desired);
+}
 
 extern void basic_test_atomics();
 
