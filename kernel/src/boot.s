fac6a7a5fbf638ac4cbb3a544b9a52eabcf78047
Christopher Hill <hill@cs.utexas.edu>
2025-02-01 19:52:34 -0600

    added multiple core booting sequence

--- kernel/src/boot.s
+++ kernel/src/boot.s
@@ -1,7 +1,33 @@
 .section ".text.boot"
 
+.extern stack0_top
 .globl _start
 _start:
-  ldr x5, =_start
+  ldr x5, =stack0_top
+  ldr x5, [x5]
   mov sp, x5
   bl kernelMain
+
+.extern stack1_top
+.globl _start_core1
+_start_core1:
+  ldr x5, =stack1_top
+  ldr x5, [x5]
+  mov sp, x5
+  bl kernelMain_core1
+
+.extern stack2_top
+.globl _start_core2
+_start_core2:
+  ldr x5, =stack2_top
+  ldr x5, [x5]
+  mov sp, x5
+  bl kernelMain_core2
+
+.extern stack3_top
+.globl _start_core3
+_start_core3:
+  ldr x5, =stack3_top
+  ldr x5, [x5]
+  mov sp, x5
+  bl kernelMain_core3
--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,3 +1,6 @@
+// Citations
+// https://medium.com/applied/applied-c-align-array-elements-32af40a768ee
+
 #include "kernel.h"
 
 #include "stdint.h"
@@ -6,15 +9,56 @@
 #include "atomics.h"
 #include "event_loop.h"
 
+#define STACK_SIZE 8192
+
+uint8_t stack0[STACK_SIZE] __attribute__((aligned(16)));
+extern "C" uint8_t* stack0_top;
+uint8_t* stack0_top = (stack0 + STACK_SIZE);
+
+uint8_t stack1[STACK_SIZE] __attribute__((aligned(16)));
+extern "C" uint8_t* stack1_top;
+uint8_t* stack1_top = (stack1 + STACK_SIZE);
+
+uint8_t stack2[STACK_SIZE] __attribute__((aligned(16)));
+extern "C" uint8_t* stack2_top;
+uint8_t* stack2_top = (stack2 + STACK_SIZE);
+
+uint8_t stack3[STACK_SIZE] __attribute__((aligned(16)));
+extern "C" uint8_t* stack3_top;
+uint8_t* stack3_top = (stack3 + STACK_SIZE);
+
+extern "C" void _start_core1();
+extern "C" void _start_core2();
+extern "C" void _start_core3();
+
+extern "C" void kernelMain_core1()
+{
+  debug_print("Core 1!\n");
+}
+
+extern "C" void kernelMain_core2()
+{
+  debug_print("Core 2!\n");
+}
+
+extern "C" void kernelMain_core3()
+{
+  debug_print("Core 3!\n");
+}
+
 extern "C" void kernelMain()
 {
   init_uart();
 
   debug_print("DingOS is Booting!\n");
 
+  // Boot other cores
+  uint64_t* core_wakeup_base = (uint64_t*) 216;
+  *(core_wakeup_base + 1) = (uint64_t) &_start_core1;
+  *(core_wakeup_base + 2) = (uint64_t) &_start_core2;
+  *(core_wakeup_base + 3) = (uint64_t) &_start_core3;
 
+  // event_loop_test();
 
-  event_loop_test();
-
-  // while(1);
+  while(1);
 }
