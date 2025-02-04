18c47d0f30c9f14c64d59a7c879c55343a8a2b60
Yifan Ma <yifanma@cs.utexas.edu>
2025-02-03 18:40:31 -0600

    Changed Event implementation to use class template

--- kernel/include/event_loop.h
+++ kernel/include/event_loop.h
@@ -1,14 +1,34 @@
 #ifndef EVENT_LOOP_H
 #define EVENT_LOOP_H
 
-// Dependencies
-struct Queue;
+struct Event {
+  // General Event data goes here
 
-typedef void (*Work)(void *);
+  explicit inline Event() {}
+  virtual void run() = 0;
+  virtual ~Event() {}
+};
 
-typedef struct {
-  Work work;
-} Event;
+template <typename Work>
+struct EventWithWork : public Event {
+  Work const work;
+
+  explicit inline EventWithWork(Work const work) : Event(), work(work) {}
+  virtual void run() override {
+    work();
+  }
+};
+
+template <typename Work, typename T>
+struct EventWithWorkAndValue : public EventWithWork<Work> {
+  T *value;
+
+  explicit inline EventWithWorkAndValue(Work const work) : EventWithWork<Work>(work) {}
+  virtual void run() override {
+    work(value);
+     // delete value; need memory management
+  }
+};
 
 void event_loop(struct Queue *queue);
 void event_loop_test();
--- kernel/src/event_loop.cpp
+++ kernel/src/event_loop.cpp
@@ -3,21 +3,14 @@
 #include "queue.h"
 #include "printf.h"
 
+
+
 void event_loop(Queue *queue) {
   Debug::printf("Entering event loop!\n");
-  while (!empty(queue)) {
-    Work event = (Work) dequeue(queue);
-    event(0);
-  }
-}
-
-void fun1(void *args) {
-  Debug::printf("In fun1()\n");
-}
-
-void fun2(void *args) {
-  Debug::printf("In fun2(), calling fun1()\n");
-  fun1(0);
+  // while (!empty(queue)) {
+  //   Work event = (Work) dequeue(queue);
+  //   event(0);
+  // }
 }
 
 void event_loop_test() {
@@ -25,9 +18,9 @@ void event_loop_test() {
   // Don't have malloc so have to do sketchy stuff
   Queue queue;
 
-  Node fun2_node = { (void*)fun2, nullptr };
-  Node fun1_node = { (void*)fun1, &fun2_node };
-  queue.head = &fun1_node;
+  // Node fun2_node = { (void*)fun2, nullptr };
+  // Node fun1_node = { (void*)fun1, &fun2_node };
+  // queue.head = &fun1_node;
 
   event_loop(&queue);
 }
