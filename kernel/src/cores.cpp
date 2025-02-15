3af241f16a05fa7bbb77defa28cf71c672d6fea8
Joshua Clark <joshuarc@utexas.edu>
2025-02-15 16:30:27 -0600

    Basic SMP that now includes cores stuff and `whichCore()` (#62)

--- kernel/src/cores.cpp
+++ kernel/src/cores.cpp
@@ -6,6 +6,11 @@
 #include "printf.h"
 #include "stdint.h"
 
+/**
+ * @brief Symmetric Multiprocessing (SMP) aka multicore support
+ * 
+ */
+namespace SMP {
 Atomic<int> startedCores = Atomic<int>(0);
 
 uint8_t stack0[STACK_SIZE] __attribute__((aligned(16)));
@@ -29,21 +34,21 @@ extern "C" void _start_core2();
 extern "C" void _start_core3();
 
 extern "C" void initCore1() {
-  Debug::printf("Core 1! %s\n", STRING_EL(get_CurrentEL()));
+  Debug::printf("Core %d! %s\n", whichCore(), STRING_EL(get_CurrentEL()));
   startedCores.add_fetch(1);
 
   event_loop();
 }
 
 extern "C" void initCore2() {
-  Debug::printf("Core 2! %s\n", STRING_EL(get_CurrentEL()));
+  Debug::printf("Core %d! %s\n", whichCore(), STRING_EL(get_CurrentEL()));
   startedCores.add_fetch(1);
 
   event_loop();
 }
 
 extern "C" void initCore3() {
-  Debug::printf("Core 3! %s\n", STRING_EL(get_CurrentEL()));
+  Debug::printf("Core %d! %s\n", whichCore(), STRING_EL(get_CurrentEL()));
   startedCores.add_fetch(1);
 
   event_loop();
@@ -57,3 +62,16 @@ void bootCores() {
   *(core_wakeup_base + 2) = (uint64_t)&_start_core2;
   *(core_wakeup_base + 3) = (uint64_t)&_start_core3;
 }
+
+/**
+ * @brief Gets the core number of core executing this function
+ *
+ * @return uint8_t  Core number
+ */
+uint8_t whichCore() {
+  uint64_t mpidr;
+  // Grabbing the Multiprocessor Affinity Register
+  asm volatile("mrs %0, mpidr_el1" : "=r"(mpidr));
+  return mpidr & 0xFF;
+}
+}  // namespace SMP
