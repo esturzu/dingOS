717797ce6e9798fb3b9859a6ca3610c3e80394ec
Joshua Clark <joshuarc@utexas.edu>
2025-02-17 22:33:36 -0600

    debug macro: printf for all the time output, dPrintf for only during debug ouptut

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -19,13 +19,13 @@ extern "C" void kernelMain() {
   // Handled uart Init
   CRTI::_init();
 
-  Debug::printf("CurrentEL %s\n", STRING_EL(get_CurrentEL()));
+  dPrintf("CurrentEL %s\n", STRING_EL(get_CurrentEL()));
 
   heap_init();
   init_event_loop();
 
-  Debug::printf("DingOS is Booting!\n");
-  Debug::printf("Core %d! %s\n", SMP::whichCore(), STRING_EL(get_CurrentEL()));
+  printf("DingOS is Booting!\n");
+  dPrintf("Core %d! %s\n", SMP::whichCore(), STRING_EL(get_CurrentEL()));
 
   SMP::bootCores();
 
