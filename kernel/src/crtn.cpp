01886a020f5050fb155af6341afa185822ded207
Alex Meyer <ameyer@cs.utexas.edu>
2025-02-19 18:43:41 -0600

    Move global ctor definition into linker script for clang compatability

--- kernel/src/crtn.cpp
+++
@@ -1,9 +0,0 @@
-// Global Constructors from
-// https://wiki.osdev.org/Calling_Global_Constructors (Public Domain)
-
-typedef void (*func_ptr)();
-
-func_ptr _init_array_end[0] __attribute__((used, section(".init_array"),
-                                           aligned(sizeof(func_ptr)))) = {};
-func_ptr _fini_array_end[0] __attribute__((used, section(".fini_array"),
-                                           aligned(sizeof(func_ptr)))) = {};
