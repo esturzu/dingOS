6192bf0b04c156384d5cf44dd0a52fadee46d45a
Yifan Ma <yifanma@cs.utexas.edu>
2025-02-25 02:13:16 -0600

    Wrote a script that runs until the output is unexpected

--- run_until_unexpected.sh
+++ run_until_unexpected.sh
@@ -3,33 +3,51 @@
 # This script will continuously run 'make qemu' until the last line of
 # kernel output is different than expected.
 
-MAX_SECS=5   # How many seconds to let QEMU run each time before timing out
+MAX_SECS=3                      # How many seconds to let QEMU run each time before timing out
+EXPECTED_LINE="HERE 4"          # Change this to the line you expect at the end
+LOGFILE="kernel_output.log"     # File to kernel output
+
 COUNTER=0
 
 while true; do
+  # Remove old logs
+  rm -f "$LOGFILE"
+
+  # Run QEMU for MAX_SECS seconds
+  # Need --foreground otherwise QEMU arguments won't be passed in
+  timeout --foreground ${MAX_SECS}s make QEMU_LOG="$LOGFILE" qemu
+
+  # Reset terminal back to cooked mode
+  reset
+
   COUNTER=$((COUNTER + 1))
   echo "=============================================="
   echo "Run #$COUNTER"
   echo "=============================================="
 
-  # Remove old logs
-  rm -f kernel/kernel_output.log
+  # Find the PID of a qemu-system-aarch64 process
+  PID=$(pgrep -f qemu-system-aarch64)
 
-  # Run QEMU via "make qemu" for MAX_SECS seconds
-  # All kernel Output is captured to kernel/kernel_output.log
-  timeout ${MAX_SECS}s make qemu
+  # Kill the QEMU process running in the background since
+  # timeout doesn't kill everything
+  if [ -n "$PID" ]; then
+    echo "Found QEMU PID: $PID"
+    kill "$PID"
+  else
+    echo "No matching QEMU process found."
+  fi
 
-  # Grab the last line of QEMU's output
-  LAST_LINE="$(tail -n 1 qemu_output.log)"
+  # Grab the last line in the kernel output file
+  LAST_LINE="$(tail -n 1 "$LOGFILE")"
 
   echo "Last line of output: '$LAST_LINE'"
 
-  # If it's not "HERE 4", we assume we've hit the deadlock scenario
-  if [ "$LAST_LINE" != "HERE 4" ]; then
-    echo "Deadlock (or unexpected exit) detected in run #$COUNTER!"
+  # If it's not the expected line, something unexpected happened
+  if [ "$LAST_LINE" != "$EXPECTED_LINE" ]; then
+    echo "Unexpected behavior detected in run #$COUNTER!"
     echo "Exiting..."
     exit 1
   fi
 
-  echo "Run #$COUNTER was successful (i.e. ended with 'HERE 4'). Retrying..."
+  echo "Run #$COUNTER was successful (i.e., ended with '$EXPECTED_LINE'). Retrying..."
 done
