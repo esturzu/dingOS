fca9f1e10c3fe386aaa875246a68bceabde4c171
Yifan Ma <yifanma07@gmail.com>
2025-04-17 03:30:21 -0500

    Re-formatted tester.h

--- kernel/include/tester.h
+++ kernel/include/tester.h
@@ -1,23 +1,21 @@
 #ifndef TESTER_H
 #define TESTER_H
 
-#include "bfsTests.h"
 #include "cores.h"
+#include "elfTests.h"
 #include "eventTests.h"
 #include "hashmapTests.h"
 #include "heapTests.h"
-#include "sdTests.h"
-#include "elfTests.h"
 #include "primitives_tests.h"
+#include "sdTests.h"
 
 void runTests() {
   elfTests();
   eventLoopTests();
   // hashmapTests();
 
-  // when running the bfs tests, you have to remake test.dd so that it isn't messed up
-  // also calling delete makes heap dead hang generally
-  // sdTests();  
+  // when running the bfs tests, you have to remake test.dd so that it isn't
+  // messed up also calling delete makes heap dead hang generally sdTests();
   // Must be done last until free is implemented
   heapTests();
   primitives_tests();
