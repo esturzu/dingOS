754c5c816109bef7271cea83a7388b6be7ac0567
Yifan Ma <yifanma07@gmail.com>
2025-04-18 12:46:31 -0500

    Cleaned up fs tests in kernelMain()

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -45,58 +45,59 @@ extern "C" void kernelMain() {
 
   SD::init();
 
-  // // I run this with this command make clean-fs;make fs-image;clear; make
-  // clean qemu DEBUG_ENABLED=0 to have the right disk, you also have to mkdir
-  // fs_root beforehand
-  //   SDAdapter* adapter = new SDAdapter(1024);
-  //   Ext2* fs = new Ext2(adapter);
-  //   const char* existing_file_name = "hello.txt";
-  //   Node* existing_test_file = find_in_directory(fs->root,
-  //   existing_file_name); int file_size = existing_test_file->size_in_bytes();
-  //       if (existing_test_file) {
-  //           char buffer[file_size + 1];
-  //           int bytes_read = read_file(existing_test_file, buffer,
-  //           file_size); if (bytes_read > 0) {
-  //             buffer[bytes_read] = '\0';
-  //             printf("File contents: %s\n", buffer);
-  //           }
-  //           delete existing_test_file;
-  //       }
-  //   // Create a test file
-  //   const char* test_filename = "example.txt";
-  //   Node* test_file = create_file(fs->root, test_filename);
+#if 0
+  // To have the right disk, you have to 'mkdir fs_root'
+  // Then, run with command:
+  // make clean-fs ; make fs-image ; clear ; make clean qemu DEBUG_ENABLED=0
+  SDAdapter* adapter = new SDAdapter(1024);
+  Ext2* fs = new Ext2(adapter);
+  const char* existing_file_name = "hello.txt";
+  Node* existing_test_file = find_in_directory(fs->root, existing_file_name);
+  int file_size = existing_test_file->size_in_bytes();
+  if (existing_test_file) {
+    char buffer[file_size + 1];
+    int bytes_read = read_file(existing_test_file, buffer, file_size);
+    if (bytes_read > 0) {
+      buffer[bytes_read] = '\0';
+      printf("File contents: %s\n", buffer);
+    }
+    delete existing_test_file;
+  }
+  // Create a test file
+  const char* test_filename = "example.txt";
+  Node* test_file = create_file(fs->root, test_filename);
 
-  //   if (test_file) {
-  //       const char* content = "Hello from DingOS EXT2 filesystem!";
-  //       test_file->write_all(0, strlen_ext(content), (char*)content);
-  //       printf("Successfully wrote to %s\n", test_filename);
-  //       delete test_file;
+  if (test_file) {
+    const char* content = "Hello from DingOS EXT2 filesystem!";
+    test_file->write_all(0, strlen_ext(content), (char*)content);
+    printf("Successfully wrote to %s\n", test_filename);
+    delete test_file;
 
-  //       // Read the file back
-  //       Node* reading_test_file = find_in_directory(fs->root, test_filename);
-  //       int file_size = reading_test_file->size_in_bytes();
-  //       if (reading_test_file) {
-  //           char buffer[file_size + 1];
-  //           int bytes_read = read_file(reading_test_file, buffer, file_size);
-  //           if (bytes_read > 0) {
-  //             buffer[bytes_read] = '\0';
-  //             printf("File contents: %s\n", buffer);
-  //           }
-  //           delete reading_test_file;
-  //       }
-  //   }
+    // Read the file back
+    Node* reading_test_file = find_in_directory(fs->root, test_filename);
+    int file_size = reading_test_file->size_in_bytes();
+    if (reading_test_file) {
+      char buffer[file_size + 1];
+      int bytes_read = read_file(reading_test_file, buffer, file_size);
+      if (bytes_read > 0) {
+        buffer[bytes_read] = '\0';
+        printf("File contents: %s\n", buffer);
+      }
+      delete reading_test_file;
+    }
+  }
+#endif
 
   setupTests();
 
-  // while (true) {
-  // }
+#if 0
+  schedule_event([] {
+    Process* proc = new Process();
+    proc->run();
+  });
 
-  //   schedule_event([]{
-  //     Process* proc = new Process();
-  //     proc->run();
-  //   });
-
-  // event_loop();
+  event_loop();
+#endif
 
   // Request a framebuffer at 640x480x32
   FrameBufferInfo* fb = framebuffer_init(640, 480, 32);
