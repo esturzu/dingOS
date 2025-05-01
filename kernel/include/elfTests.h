d446da8e608081b983af5e1b28a6cca6bdb0979c
Yuvraj Sahu <ysahu.atx@gmail.com>
2025-05-01 10:46:51 -0500

    bug fixes to elf tests; removal of debug output in heap tests

--- kernel/include/elfTests.h
+++ kernel/include/elfTests.h
@@ -6,8 +6,8 @@
 #include "testFramework.h"
 
 typedef uint32_t (*SampleProcessFunction)();
-constexpr size_t NUM_INSTRUCTIONS = 12;
-uint64_t PROGRAM_LOAD_LOC = 0x0000'0001'0000'0000;
+constexpr size_t NUM_INSTRUCTIONS = 7;
+uint64_t PROGRAM_LOAD_LOC = 0xFFFF'0000'0000'0000;
 
 struct ELFFile {
   ELFLoader::ELFHeader64 header;
@@ -42,22 +42,6 @@ const ELFFile ELF_FILE = (ELFFile) {
 
   // Machine Code (Text)
   {
-    0xD10043E0, // sub x0, sp, #16
-    0x52800D01, // mov w1, #104
-    0x52800D22, // mov w2, #105
-    0x52800143, // mov w3, #10
-    0x39000001, // strb w1, [x0]
-    0x39000402, // strb w2, [x0, #1]
-    0x39000803, // strb w3, [x0, #2]
-    0xD2800061, // mov x1, #3
-    0xD2800022, // mov x2, #1
-    0xD4000101, // svc #0x08
-    0xD2800000, // mov x0, #0
-    0xD4000001  // svc #0x00
-  }
-
-  /*
-  {
     0xD2800D00, // mov x0, 104 // 'h'
     0xD4000041, // svc 2
     0xD2800D20, // mov x0, 105 // 'i'
@@ -66,7 +50,6 @@ const ELFFile ELF_FILE = (ELFFile) {
     0xD4000041, // svc 2
     0xD4000001  // svc 0
   }
-  */
 };
 
 /*
@@ -108,20 +91,21 @@ void elfTests() {
   char* data = (char*) &ELF_FILE;
   constexpr size_t size = sizeof(ELF_FILE);
 
-  Process* process = new Process();
+  Process* process = (Process*) malloc(sizeof(Process));
   ELFLoader::Result result = ELFLoader::load(data, size, process);
   bool success = result.success();
   testsResult("ELF Load 1", success);
+  if (!success) return;
 
-  if (success) {
-    schedule_event([process] () { process->run(); });
-    unsigned long value = 12345;
-    for (unsigned long i = 0; i < (1LU << 28); i++) value = value * 678 + 90;
-    printf(" ELF should have printed 'hi' above\n");
-    printf(" ELF Run Random Result: %lu\n", value);
+  schedule_event([process] () { process->run(); });
+  unsigned long value = 12345;
+  for (unsigned long i = 0; i < 200'000'000; i++) {
+    value *= 54321;
+    value += 67890;
   }
 
-  delete process;
+  printf(" ELF should have printed 'hi' above\n");
+  printf(" ELF Run Random Result: %lu\n", value);
 }
 
 #endif // ELF_TESTS_H
